e69504bcbaa34d95f073fa6b47e31a5e0ac933ad
sjors <sjors.pol@outlook.com>
2025-04-17 19:29:23 +0200

    first setup

---
+++ src/main/java/com/quadsjors/triviaapi/TriviaController.java
@@ -0,0 +1,4 @@
+package com.quadsjors.triviaapi;
+
+public class TriviaController {
+}
---
+++ src/test/java/com/quadsjors/triviaapi/TriviaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quadsjors.triviaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
